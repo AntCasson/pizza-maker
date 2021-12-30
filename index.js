b1777ebddd3a359e17c64848ac302b8257e8bfc6
AntCasson <anthonyrcasson@gmail.com>
2021-12-30 15:15:12 +0100

    Clean up JS file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const pizzaSelect = document.querySelector("#pizza-select")
 const pizzaDisplay = document.querySelector("#pie-count-display")
 const poolish = document.querySelector("#poolish")
 const recipeTitle = document.querySelector("#recipe-title")
+const poolishSpan = document.querySelector("#yeast-p")
 // text that will be rendered in recipe-title
 let sizeText = ""
 
@@ -10,8 +11,10 @@ let numberOfPizzas = pizzaSelect.value
 let pizzaIcon = "🍕"
 let flourAmount = 0
 let waterAmount = 0
+let waterPercentage = 0
 let saltAmount = 0 
-let yeastAmount = 0.5 
+let yeastAmount = 0.5
+let poolishWeight = 0
 
 // Pizza size selectors btns
 const flour = document.querySelector("#flour-weight")
@@ -26,82 +29,75 @@ document.querySelectorAll(".btn-size").forEach(btn => {
         yeastAmount = 0.5
         saltAmount = pizzaSelect.value 
 
-        switch (sizePie) {
-            case "S":
-              flourAmount = 152
-              sizeText=  "small"
-              break
-            case "M":
-                flourAmount = 175
-                sizeText=  "medium"
-              break
-            case "L":
-                sizeText=  "large"
-                flourAmount = 200
-                yeastAmount = 0.6
-              break
-        }
-        flourAmount = flourAmount * numberOfPizzas
+          switch (sizePie) {
+              case "S":
+                flourAmount = 152
+                sizeText=  "small"
+                break
+              case "M":
+                  flourAmount = 175
+                  sizeText=  "medium"
+                break
+              case "L":
+                  sizeText=  "large"
+                  flourAmount = 200
+                  yeastAmount = 0.6
+                break
+          }
+        
         renderRecipe()
-        console.log(flourAmount * numberOfPizzas, waterAmount, saltAmount)
     })
 })
 
+function hydrationCalc(e) {
+  let hydration = e.target.id.slice(6, 8)
+  waterPercentage = Number.parseInt(hydration)
+  waterAmount = Math.round(flourAmount / 100 * waterPercentage)
+
+  renderRecipe()
+}
+//Even listeners for the hydration buttons
 document.querySelectorAll(".water-drop").forEach(btn => {
-    btn.addEventListener("click", (event) => {
-        let hydration = event.target.id.slice(6, 8)
-        
-        switch (hydration) {
-            case "60":
-              waterAmount = Math.round(flourAmount / 100 * 60)
-              break
-            case "65":
-                waterAmount = Math.round(flourAmount / 100 * 65)
-              break
-            case "70":
-                waterAmount = Math.floor(flourAmount / 100 * 70)
-              break
-        }
-        renderRecipe()
-    })
-})
+    btn.addEventListener("click", hydrationCalc)})
 
 pizzaSelect.addEventListener("change", () => {
     pizzaDisplay.textContent = " "
     for (let i = 0; i < pizzaSelect.value; i++) {
         pizzaDisplay.textContent += pizzaIcon
         numberOfPizzas = pizzaSelect.value
-        saltAmount = pizzaSelect.value 
-        renderRecipe()
-        console.log(pizzaIcon)
-    }
+        saltAmount = pizzaSelect.value
+      }
+
+    // poolish checked or not 
+    poolishWeight = 75 * numberOfPizzas
+    if (poolish.checked) {
+      poolishSpan.innerHTML = `Poolish <span class="text-yellow">${poolishWeight}</span> gr`}
+
+    renderRecipe()
 })
 
 function renderRecipe() {
     recipeTitle.innerHTML = `You're making ${numberOfPizzas} ${sizeText} pizza('s).`
-    water.textContent = waterAmount 
-    flour.textContent = flourAmount 
-
+    water.textContent = Math.round(flourAmount / 100 * waterPercentage) * numberOfPizzas
+    flour.textContent = Math.round(flourAmount * numberOfPizzas)
     //the 3.5 in salt is the standard amount of grams per pizza
     salt.textContent = saltAmount * 3.5
     yeast.textContent = Math.round(yeastAmount * numberOfPizzas)
+    poolishWeight = 75 * numberOfPizzas 
+    
 }
 
-poolish.addEventListener("change", () =>{
-  console.log("checked")
-  const hideYeast = document.querySelector("#yeast-p")
-  
+poolish.addEventListener("change", poolishCalc)
+
+function poolishCalc() {
   if (poolish.checked) {
-    hideYeast.innerHTML = `Poolish <span class="text-yellow">${75*numberOfPizzas}</span> gr`
-    flourAmount = flourAmount- 50 *numberOfPizzas
-    waterAmount = waterAmount- 50 *numberOfPizzas
+    poolishSpan.innerHTML = `Poolish <span class="text-yellow">${poolishWeight}</span> gr`
+    flourAmount = flourAmount- 37.5
+    waterAmount = waterAmount- 37.5 
   } else {
-    flourAmount = flourAmount+ 50 *numberOfPizzas
-    waterAmount = waterAmount+ 50 *numberOfPizzas
-    hideYeast.innerHTML = `Yeast <span id="yeast-weight">${Math.round(yeastAmount * numberOfPizzas)}</span> gr`
+    flourAmount = flourAmount+ 37.5 
+    waterAmount = waterAmount+ 37.5 
+    poolishSpan.innerHTML = `Yeast <span id="yeast-weight">${Math.round(yeastAmount * numberOfPizzas)}</span> gr`
   }
-
   renderRecipe()
-})
-
-function 
+}
